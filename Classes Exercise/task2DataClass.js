397465e919604b6037000e442adb9acced7214a2
ba77on <78099942+ba77on@users.noreply.github.com>
2022-09-04 16:37:53 +0300

    Add files via upload

---
+++ Classes Exercise/task2DataClass.js
@@ -0,0 +1,17 @@
+class Request {
+    constructor(method, uri, version, message) {
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
+        this.response = undefined;
+        this.fulfilled = false;
+    }
+}
+
+// • method (String)
+// • uri (String)
+// • version (String)
+// • message (String)
+// • response (String) -undefined
+// // • fulfilled (Boolean) - false
