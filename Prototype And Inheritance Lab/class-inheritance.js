0d079c1e7c42848e6190d1b1e50d630ba2af35e3
ba77on <78099942+ba77on@users.noreply.github.com>
2022-10-02 23:20:49 +0300

    Add files via upload

---
+++ Prototype And Inheritance Lab/class-inheritance.js
@@ -0,0 +1,30 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hi, my name is ${this.name}`);
+    }
+}
+
+class Employee extends Person {
+    constructor(name, age, department, id) {
+        super(name, age);
+
+        this.department = department;
+        this.id = id;
+    }
+
+    showId() {
+        console.log(`${this.name} has an Id of ${this.id}`);
+    }
+}
+
+let person = new Person('Gosho', 10);
+let employee = new Employee('Pesho', 20, 'IT', 111);
+person.greet();
+employee.greet();
+
+employee.showId();
