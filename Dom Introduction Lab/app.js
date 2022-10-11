9f70120d637142f2f65ca1b08a134f40b0090389
ba77on <78099942+ba77on@users.noreply.github.com>
2022-10-11 22:34:02 +0300

    Delete app.js

--- Dom Introduction Lab/app.js
+++
@@ -1,9 +0,0 @@
-function addItem() {
-    let listOfItems = document.getElementById("items");
-    let resultInput = document.getElementById("newItemText").value;
-    let liElement = document.createElement('li');
-    liElement.textContent = resultInput;
-
-    listOfItems.appendChild(liElement);
-    document.getElementById("newItemText").value = '';
-}
