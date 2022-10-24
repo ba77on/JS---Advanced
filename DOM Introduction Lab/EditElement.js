1a09dbb22da8e7999e0849a7e1a384ff7fc684c6
ba77on <78099942+ba77on@users.noreply.github.com>
2022-10-24 21:34:34 +0300

    Delete EditElement.js

--- DOM Introduction Lab/EditElement.js
+++
@@ -1,6 +0,0 @@
-function editElement(ref, match, replacer) {
-    let content = ref.textContent;
-    let matcher = new RegExp(match, 'g');
-    let edited = content.replace(matcher, replacer);
-    ref.textContent = edited;
-}
